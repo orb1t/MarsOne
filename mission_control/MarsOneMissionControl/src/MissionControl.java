268a1c1401f011bff1debd3d0fd0a7f9537312dc
Zachary Balda <zacharybalda23@gmail.com>
2017-01-21 13:31:15 -0500

    Created framework

---
+++ mission_control/MarsOneMissionControl/src/MissionControl.java
@@ -0,0 +1,12 @@
+
+public class MissionControl {
+	
+	public void initializeData(){
+		System.out.println("Data Initialized from Mission Control.");
+	}
+	
+	public void updateData(){
+		System.out.println("Data Updated from Mission Control");
+	}
+	
+}
