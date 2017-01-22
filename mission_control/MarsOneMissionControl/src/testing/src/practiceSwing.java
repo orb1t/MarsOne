6311e0cbeba442b61e78bab66a5835e0877f121d
nrkroeker <nathaliekroeker@gmail.com>
2017-01-22 03:57:28 -0500

    Didn't even get the clock working

---
+++ mission_control/MarsOneMissionControl/src/testing/src/practiceSwing.java
@@ -0,0 +1,26 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.*;
+
+public class practiceSwing extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args){
+		
+		new practiceSwing();
+		
+	}
+	
+	public practiceSwing(){
+		
+		this.setSize(400,400);
+		
+		this.setVisible(true);
+		
+	}
+	
+}
